001766c7dd2b22c5b083a6749f23f23b0db0912e
Rares Chelariu <Rares.Chelariu@nuvei.com>
2023-10-07 14:35:27 +0300

    compute pattern only when match is called (otherwise stack overflow for ctor)

--- Json.Validator/Value.cs
+++ Json.Validator/Value.cs
@@ -4,19 +4,22 @@ namespace Json;
 
 public class Value : IPattern
 {
-    private readonly IPattern _pattern = GetValuePattern();
-
     private static Choice GetValuePattern()
     {
         var stringPattern = GetStringPattern();
         var number = GetNumberPattern();
-        
+        var obj = new Object();
+        var arr = new Arr();
+
         var value = new Choice(
-            stringPattern,
-            number,
             new TextPattern("true"),
             new TextPattern("false"),
-            new TextPattern("null"));
+            new TextPattern("null"),
+            stringPattern,
+            number,
+            obj,
+            arr
+        );
 
         return value;
     }
@@ -30,7 +33,7 @@ public class Value : IPattern
     {
         return new StringPattern();
     }
-    
+
     public IMatch Match(string text)
     {
         if (string.IsNullOrEmpty(text))
@@ -38,6 +41,7 @@ public class Value : IPattern
             return new Match(false, text);
         }
 
-        return _pattern.Match(text);
+        var pattern = GetValuePattern();
+        return pattern.Match(text);
     }
 }
