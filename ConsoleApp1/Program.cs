4a3afde6a009101c858100adb2670f0e18495c03
Rares Chelariu <Rares.Chelariu@nuvei.com>
2023-10-07 17:02:57 +0300

    fix number to return true even when there is remaining text

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,6 @@
-﻿using Json;
-
-var path = Path.Combine(Directory.GetCurrentDirectory(), "input.json");
+﻿var path = Path.Combine(Directory.GetCurrentDirectory(), "input.json");
 var text = File.ReadAllText(path);
 
-//var pattern = new Value();
-
-// var pattern = new StringPattern();
 var pattern = new Json.Object();
 var match = pattern.Match(text);
 if (match.Success())
--- Json.Validator/Basics/Number.cs
+++ Json.Validator/Basics/Number.cs
@@ -2,24 +2,33 @@
 
 public class Number : IPattern
 {
-    private readonly IPattern pattern;
+    private readonly IPattern _pattern;
 
     public Number()
     {
+        var maybeMinus = new OptionalPattern(new Character('-'));
+        var zero = new Character('0');
         var digits = new OneOrMore(new Range('0', '9'));
+        var nonZeroDigit = new Range('1', '9');
+        var dot = new Character('.');
+        var eSign = new Any("eE");
+        var sign = new Any("+-");
+        
         var integer = new Choice(
-            new Sequence(new OptionalPattern(new Character('-')), new Character('0')),
-            new Sequence(new OptionalPattern(new Character('-')), new Sequence(new Range('1', '9'), new Many(digits))));
-        var fraction = new Sequence(new Character('.'), digits, new OptionalPattern(new Many(digits)));
-        var exponent = new Sequence(new Any("eE"), new OptionalPattern(new Any("+-")), digits);
-        pattern = new Sequence(integer, new OptionalPattern(fraction), new OptionalPattern(exponent));
+            new Sequence(maybeMinus, zero),
+            new Sequence(maybeMinus, new Sequence(nonZeroDigit, new Many(digits))));
+        
+        var fraction = new Sequence(dot, digits, new OptionalPattern(new Many(digits)));
+        
+        var exponent = new Sequence(eSign, new OptionalPattern(sign), digits);
+        
+        _pattern = new Sequence(integer, new OptionalPattern(fraction), new OptionalPattern(exponent));
     }
 
     public IMatch Match(string text)
     {
-        var match = pattern.Match(text);
-        return match.Success() && string.IsNullOrEmpty(match.RemainingText())
-            ? match
-            : new Match(false, text);
+        var match = _pattern.Match(text);
+        
+        return match;
     }
 }
